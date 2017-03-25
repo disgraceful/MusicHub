8213d8348772bada7cea96d363cfbf95913c9b76
disgraceful <noolic97@gmail.com>
2017-03-25 16:25:46 +0200

    Add rest user controller

--- src/main/java/com/mymedia/web/mvc/controller/UserRestController.java
+++ src/main/java/com/mymedia/web/mvc/controller/UserRestController.java
@@ -1,46 +1,35 @@
 package com.mymedia.web.mvc.controller;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mymedia.web.mvc.model.AuthUser;
 import com.mymedia.web.service.AuthUserService;
-import com.mymedia.web.utils.SpringBeanProvider;
 
-@Controller
-@RequestMapping("/authusers")
-public class AuthUserController {
+@RestController
+public class UserRestController {
 
 	private static final Logger LOG = LogManager.getLogger(AuthUserController.class);
 
-	private static AuthUserService getService() {
-		return SpringBeanProvider.getBean(AuthUserService.class);
-	}
+	@Autowired
+	AuthUserService userService;
 
-	@RequestMapping(value = "/getuser/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
 	public @ResponseBody AuthUser getAuthUserByID(@PathVariable int id) {
-		AuthUser a = getService().getAuthUser(id);
+		AuthUser a = userService.getAuthUser(id);
 		LOG.info(a.toString());
-
 		return a;
 	}
-
-	@RequestMapping(value = "/listusers", method = RequestMethod.GET)
-	public @ResponseBody List<AuthUser> listAuthUsers() {
-		return getService().getAllAuthUsers();
-	}
-
+	
 	@RequestMapping(value="/adduser",method=RequestMethod.POST)
 	public @ResponseBody AuthUser postUser(@RequestBody AuthUser user) {
-		return getService().addAuthUser(user);
+		return userService.addAuthUser(user);
 	}
-	
 }
